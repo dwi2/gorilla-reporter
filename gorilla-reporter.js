ebf5290eb1e4ca140e53666a81567f52b15d2904
dwi2 <dwi2@dwi2.com>
2019-03-19 21:23:45 +0900

    Add prettier to format code and tests and make it running

---
+++ gorilla-reporter.js
@@ -0,0 +1,70 @@
+'use strict';
+const mocha = require('mocha');
+
+const BANANA =`
+        .-.
+       /  |
+      |  /
+   .'\\|.-; _
+  /.-.;\\  |\\|
+  '   |'._/ \`
+      |  \\
+       \\  |
+        '-'
+`;
+
+function write(text) {
+  process.stdout.write(text);
+}
+
+function gorillaSays(text) {
+  const words = text ? text : '';
+  write(`
+           ."\`".
+       .-./ _=_ \\.-.  ${words}
+      {  (,(oYo),) }}   )
+      {{ |   "   |} }  /
+      { { \\(---)/  }}
+      {{  }'-=-'{ } }
+      { { }._:_.{  }}
+      {{  } -:- { } }
+      {_{ }\`===\`{  _}
+     ((((\\)     (/))))
+  `)
+}
+
+function GorillaReporter(runner) {
+  mocha.reporters.Base.call(this, runner);
+  var passes = 0;
+  var failures = 0;
+
+  runner.on('start', () => {
+    gorillaSays(`哥! 不要緊張~~`);
+    write('\n');
+  });
+
+  runner.on('pass', (test) => {
+    passes++;
+    write(`A_A\t${test.fullTitle()}\n`);
+  });
+
+  runner.on('fail', (test, err) => {
+    failures++;
+    write('\n');
+    write(`幹: ${test.fullTitle()} -- 這個錯了啦: ${err.message}\n`);
+  });
+
+  runner.on('end', () => {
+    write('\n');
+    if (failures > 0) {
+      write(`${passes + failures}個裡面錯了${failures}個, 我要被你笑死!
+      `);
+    } else {
+      write(BANANA);
+      write(`${passes}/${passes + failures} 全部都對了! 很不錯耶~~`);
+    }
+    write('\n');
+  });
+}
+
+module.exports = GorillaReporter;
---
+++ test/basic.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+describe('Basic', () => {
+  it('should print test passed information', (done) => {
+    setTimeout(() => {
+      assert.equal(1 + 1, 2);
+      done();
+    }, 1000);
+
+  });
+
+  it('should not able to print additional message yet', (done) => {
+    setTimeout(() => {
+      assert.equal(42, 42, '額');
+      done();
+    }, 1000);
+  });
+
+  it('should print swearing when tests fail', () => {
+    assert.fail('我要崩潰了!');
+  });
+}).timeout(3000);
